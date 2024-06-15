4f934447662d66999e1b52105bde2cdac642119a
Ravindra Kumar Modpatel <ravi.rhce@gmail.com>
2024-06-15 07:59:30 +0530

    adding files

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "admin_username" {
+    description = "value of the admin username"
+    type = string
+    sensitive = true
+}
+
+variable "admin_password" {
+    description = "value of the admin password"
+    type = string
+    sensitive = true
+  
+}
